87d72b5faacbe0ae2db5abcdc6cbde0238e22344
Asif Jirayat <asifjirayat@gmail.com>
2025-08-10 20:20:43 +0530

    Added copy to clipboard onclicking a swatch

--- src/components/ColorSwatch.jsx
+++ src/components/ColorSwatch.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+
 export default function ColorSwatch({ color, label, isBase = false }) {
+  const [isCopied, setisCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(color.toUpperCase());
+      setisCopied(true);
+      setTimeout(() => {
+        setisCopied(false);
+      }, 1500);
+      console.log(color + " copied to clipboard");
+    } catch (err) {
+      console.log("Failed to copy: ", err);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center space-y-1">
+    <div
+      onClick={handleCopy}
+      title="Click to copy"
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === "") {
+          handleCopy();
+        }
+      }}
+      className="flex flex-col items-center space-y-1 cursor-pointer group"
+    >
       {/* Color Square */}
       <div
         className={`w-16 h-16 border-2 rounded-lg shadow-sm transition-all duration-150 ${
